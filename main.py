3edcf23aab58ac90b9d848c1e011476a9cce85e2
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-23 14:18:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 from pyrogram import Client, filters, idle
 
-SESSION = "AgAhIHQAlKSxKdju-HtHsJl62CvRASw879V3r5n9G5jGdwzJELydKm5Bd5wtxwL4noZR03_tzazlZYGhFRWW_K_-gWKMI_wvOTtoK0kGYbborMS1pK74l82WIkbOC01ZA28_k59kCAyG4MOgkdgImWNJKHG5dVeNfHxZxR0H5UpfwzT7ww8npADxCBzbPDexioJ-zozECfRG1X1pCY7tBXPVC7BhyArFiYiGuEcw4eJ9PiUg9iNQtdIoCVJicK7Su5es-k3LNTS4SxLMYmPDJ3gGXyZhPmQsW2JUn9MinemlbBD4-9S9b2Z9xhQhMcm0kpySNGT-mQRHnt3gZrlEoX607XI3vQAAAAAxrBH9AA" # pyrogram string session 
-API_ID = 12962251 # Your API_ID Get it from my.telegram.org
-API_HASH = 'b51499523800add51e4530c6f552dbc8' # Your API_HASH Get it from my.telegram.org
+SESSION = "AgFPyocATVJPWW9yp0nOMq4mR9WgAJUwQKQw3Afa50BnEV3qmtK1efAl-QYYEk6UBPTLknY9ObjToNnxRY62hXpR18eC1CF3lr88-BmN0G43qk90TQ0t0ef_6BTWQm_RcB3mAIQidSKBHcrIDgYbOK3F2he-qmtU14MauorhS-H3MedV5SfcU4uwSmBxZPglOdpgSyWQ8OwWg43uz9egLN9I8UWXScEA_Einqct0SNnLn71-yffUHOx4hIfv_pkd9VZWvf6yQg0nPX2hcTUUJuuaOjQplfTT9FDmJnep2lRjTMJUzBqBOx4mfRZwMNi2Rp3F_QlGQ5e0YJ6KHBO_TDnPM2-tVgAAAAFM8cI7AA" # pyrogram string session 
+API_ID = 22006407 # Your API_ID Get it from my.telegram.org
+API_HASH = '62c5a41a1a6e9973edbe920fbf90e5b3' # Your API_HASH Get it from my.telegram.org
 
 app = Client(
       'session_name',
