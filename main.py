5851fd35681bb79787a06d8631f8fe1a6c221b10
z75z <110237161+z75z@users.noreply.github.com>
2024-11-26 22:11:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
-# @DNDZAiD - @Y88F8
+import os
 
-from pyrogram import Client, filters, idle
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-SESSION = "AgGi8fAAB72dv--vzkZzaX907GyutBRk7j5j_Xxpy4CV4l9pXalGzUpAc9b85IsC3qwot_J0InblDpwUF47mtm-QyL1SU9HaMvdfttUg7BkZQjkcW6lXu-1SfVxojcS6CDHp56shf2ecltrSeILC_rDKdC1Kta0wAvwxb5iV5D-yWcKGbjDmgB_UT4ADjJYYaANy4FwDPwf2RyYQwhVLfryS_FUj9TvEnX4kVnKuB1fg_HqJ7G_fraa1dj1b0GgUZcEqBKYztaRci7mI3Z_6YV0l2_4AsmSk7UoFMGfugweElo4YmFOKY3wyMmJMqo69x_O66YzBlhkHmV2cZuMTUaImEjJktwAAAAFM8cI7AA" # pyrogram string session 
-API_ID = 22006407 # Your API_ID Get it from my.telegram.org
-API_HASH = '62c5a41a1a6e9973edbe920fbf90e5b3' # Your API_HASH Get it from my.telegram.org
+
+SESSION = "AgDFycsAqcy6mB2G7iXEs4T_hO0njqBnl7UAaqKGaAtq30t9S6lwRBja1vDTwFJ62Sm_gm7X0ei4ULZeG-S7WzMXjCGsIm7HnFiN05O9LRbY_pbPDvyNsgr5KwZykcv434uZuTYzN5AtZ23OPG41VXS_MBOn0Eclm_-_8nY_ehcRIPaJlDZnNac95SKytETzvbuqYuzPvh8etTRT5-Hk09obPxUBilYiaP0wsO8S-Vhn5SRbbO8i3nNKd5WMvLxhF5f6PIz5S-6Tnpxbo9sPMAAxv4Nz4ylSeAW5dZIdZM5DS1OXtoIq61XBKTOolM2wC7Y7AVPhknGqAV3MFztPW1Geyor6BwAAAAAxrBH9AA" # pyrogram string session 
+API_ID = 12962251 # Your API_ID Get it from my.telegram.org
+API_HASH = "b51499523800add51e4530c6f552dbc8" # Your API_HASH Get it from my.telegram.org
 
 app = Client(
       'session_name',
       API_ID, API_HASH, session_string = SESSION, #in_memory=True
 )
 
-app.start() # To start Your Client
-
-@app.on_message(filters.media & filters.private)
-async def get_dest(c, m):
-    # To save Self-Destructing photo
-    if m.photo and m.photo.ttl_seconds:
-       photo = await m.download()
-       return await c.send_photo("me",photo)
-       
-    # To save Self-Destructing Video
-    if m.video and m.video.ttl_seconds:
-       video = await m.download()
-       return await c.send_video("me",video)
+@app.on_message(filters.self_destruction, group=6)
+async def save_timer_media(client: Client, message: Message):
+    try:
+        if message.media:
+            file_path = await message.download()
+            await client.send_document(
+                "me", document=file_path, caption=message.caption or "Saved timer media"
+            )
+            os.remove(file_path)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-print(f"Your Client {app.me.first_name} started successfully")
-idle()
+# Command to save a specific photo timer message
+@app.on_message(
+    filters.command(["save", "سەیڤ", "/save"], "") & filters.reply, group=7
+)
+async def save_replied_timer_photo(client: Client, message: Message):
+    try:
+        # Ensure the replied message has a media file
+        if message.reply_to_message and message.reply_to_message.media:
+            file_path = await message.reply_to_message.download()
+            await client.send_document(
+                "me",
+                document=file_path,
+                caption=message.reply_to_message.caption or "Saved replied timer photo",
+            )
+            os.remove(file_path)
+            await message.reply("اوفف")
+        else:
+            await message.reply("Please reply to a timer photo message to save it.")
+    except Exception as e:
+        print(f"Error: {e}")
+        await message.reply("Failed to save the timer photo.")
+        
+        
+        
+app.run()
